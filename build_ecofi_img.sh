04b41b26ee5e6154e33c63be4d30c6b524ab86cc
Eco-Fi Builder <ecofi@builder.local>
2026-09-05 20:39:23 +0800

    Bump build script output to v1.1

--- build_ecofi_img.sh
+++ build_ecofi_img.sh
@@ -3,13 +3,13 @@
 # ECO-Fi OS Image Rebuilder & Customizer
 # Deep Cleaning, Hardening & ECO-Fi Integration for Orange Pi One
 # Base: resources/PisoFi_Opi1&PC_v5.3.0-05-10-26_EXT.img
-# Target: resources/EcoFi_Opi_v1.0.img
+# Target: resources/EcoFi_Opi_v1.1.img
 # ==============================================================================
 
 set -e
 
 BASE_IMG="/mnt/d/PROJECTS_IO/Plastic-Bottle-Vending-Machine/resources/PisoFi_Opi1&PC_v5.3.0-05-10-26_EXT.img"
-TARGET_IMG="/mnt/d/PROJECTS_IO/Plastic-Bottle-Vending-Machine/resources/EcoFi_Opi_v1.0.img"
+TARGET_IMG="/mnt/d/PROJECTS_IO/Plastic-Bottle-Vending-Machine/resources/EcoFi_Opi_v1.1.img"
 MOUNT_DIR="/tmp/ecofi_mount"
 SOURCE_HOST="/mnt/d/PROJECTS_IO/Plastic-Bottle-Vending-Machine/host"
 
